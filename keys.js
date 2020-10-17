3d0c7216a8fa9e32842703e14d90ae3a4d262c93
Aswin A V <aswinavofficial@gmail.com>
2020-10-18 00:27:48 +0530

    Moved credentials to env variables

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGOURL: "mongodb+srv://insta:nr92PLb8xmkf794h@cluster0-g7ny5.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "@^D8SwCLkd9GuaRA"
+    MONGOURL: process.env.MONGOURL,
+    JWT_SECRET: process.env.JWT_SECRET
 }
