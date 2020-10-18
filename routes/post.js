7cca9e2c455030f6e122d5b04fb93f7874f5242f
Aswin A V <aswinavofficial@gmail.com>
2020-10-18 23:44:56 +0530

    Infinite Scroll mock api

--- routes/post.js
+++ routes/post.js
@@ -26,15 +26,15 @@ router.get('/post', requireLogin, (req, res) => {
 router.get('/post/all', requireLogin, (req, res) => {
 
     Post.find()
-        .populate("postedBy", "_id name")
-        .then(results => {
+        .populate("postedBy","_id name")
+        .then(posts => {
 
-            results.forEach(result => {
+            posts.forEach(result => {
                 result.createdAt = undefined
                 result.updatedAt = undefined
                 result.__v = undefined
             })
-            res.json({ results })
+            res.json({ posts })
         }).catch(error => {
             console.log(error)
             res.status(500).json({ error: "Internal Server error" })
@@ -43,11 +43,27 @@ router.get('/post/all', requireLogin, (req, res) => {
 
 })
 
+router.get('/post/latest', requireLogin, (req, res) => {
+
+    const postid = Math.floor(Math.random() * 100)
+    const post = {
+        _id: postid ,
+        title: "Post " + postid ,
+        body: "Some more random text",
+        postedBy: {
+            _id: "5f8c1332114fdd56dd25e65c",
+            name: "Aswin",
+            email: "aswinavofficial@gmail.com"
+        }
+    }
+
+    res.json( [post] )
+})
 
 router.post('/post', requireLogin, (req, res) => {
 
-    const { title, body } = req.body
-    if (!title || !body) {
+    const { title, body,photo } = req.body
+    if (!title || !body || !photo ) {
         return res.status(422).json({ error: "Please enter all required  fields" })
     }
 
@@ -55,7 +71,8 @@ router.post('/post', requireLogin, (req, res) => {
     const post = new Post({
         title,
         body,
-        postedBy: req.user
+        postedBy: req.user,
+        photo
     })
 
     post.save().then(result => {
